02ab4da61160b183ccc24936289832606ba0d503
nLaugas <30382191+nLaugas@users.noreply.github.com>
2020-06-27 20:22:19 -0300

    Updates

---
+++ precache-manifest.35fab8f624f27db2dc67164878404ce2.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1fea14f049a8ddf74c04af400cfd3b",
+    "url": "/WitBiz/index.html"
+  },
+  {
+    "revision": "486bf36a0e25c84c30c0",
+    "url": "/WitBiz/static/css/main.80c47550.chunk.css"
+  },
+  {
+    "revision": "665cdf687e11d823d9e9",
+    "url": "/WitBiz/static/js/2.de9c669b.chunk.js"
+  },
+  {
+    "revision": "486bf36a0e25c84c30c0",
+    "url": "/WitBiz/static/js/main.f8c31eaa.chunk.js"
+  },
+  {
+    "revision": "cb2bf0d67abdb225d6f8",
+    "url": "/WitBiz/static/js/runtime~main.22c79f11.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/WitBiz/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "840650bbb173702ed680401deb2daabe",
+    "url": "/WitBiz/static/media/bg1.840650bb.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/WitBiz/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/WitBiz/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/WitBiz/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/WitBiz/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/WitBiz/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/WitBiz/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/WitBiz/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/WitBiz/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "840650bbb173702ed680401deb2daabe",
+    "url": "/WitBiz/static/media/header.840650bb.jpg"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/WitBiz/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "f8fba45478e18757e67c37b6644a3cec",
+    "url": "/WitBiz/static/media/landing.f8fba454.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/WitBiz/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/WitBiz/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/WitBiz/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/WitBiz/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/WitBiz/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "225653f9f4dacef3e81bcc52f5ef547d",
+    "url": "/WitBiz/static/media/profile.225653f9.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/WitBiz/static/media/ryan.f217f346.jpg"
+  }
+]);
